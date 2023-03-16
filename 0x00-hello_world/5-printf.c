e0f079791562aa8f008655416b88ef21610dbd2d
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-16 18:28:17 +0100

    ll

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - This is a function that prints a string.
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
