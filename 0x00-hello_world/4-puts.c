f62266b4348043a045166d3cca885fed321009ab
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-03-16 15:52:24 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #!/bin/bash
 int main() {
   write(1, "Programming is like building a multilingual puzzle\n", strlen("Programming is like building a multilingual puzzle"));
-  return 0;
+  return(0);
 }
