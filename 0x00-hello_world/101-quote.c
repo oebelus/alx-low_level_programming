066321cb47aa177ea6a46ccaf8f7ba2274c5e9f1
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-16 18:49:26 +0100

    ll

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - This is a function that prints a string.
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (0);
+}
