b24504ade019b994df5e84216f3528e2ce456bda
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-17 20:49:37 +0100

    ll

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print the alphabet.
+ * Return: 0 if success
+*/
+
+int main(void)
+{
+	char c;
+    
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+	putchar(c);
+	}
+	return (0);
+}
