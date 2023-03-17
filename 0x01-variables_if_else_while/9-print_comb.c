b229b3d0df3734fe8592df2fdc1a78d4d1da9a38
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-17 21:24:06 +0100

    ll

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print the alphabet.
+ * Return: 0 if success
+*/
+
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+	putchar(i);
+	if (i != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	++i;
+	}
+	putchar('\n');
+	return (0);
+}
