bd6bf5a4bfeede7eeeec02298a7a44b3638c3d7c
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-03-17 14:44:54 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,5 +10,13 @@
          srand(time(0)); 
          n = rand() - RAND_MAX / 2; 
          /* your code goes there */ 
+         if (n < 0)
+         {
+          printf("n is positive")
+         }
+         else if
+         {
+          
+         }
          return (0); 
  }
