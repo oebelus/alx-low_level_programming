11f08ad2371636bd549d0e023abc78a77aa71c86
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-20 18:21:54 +0000

    ll

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print the alphabet.
+ * Return: 0 if success
+*/
+
+int main(void) {
+	int c = '0';
+
+	int d = '0';
+
+	while (c <= '9')
+	{
+		while (d <= '9')
+		{
+			if ((c < d) || c == d)
+			{
+				putchar(c);
+				putchar(d);
+				if (c == '8' && d == '9')
+				{
+					putchar("\n");
+				}
+				else
+				{
+					putchar(",");
+					putchar(" ");
+				}
+			}
+			d++;
+		}
+		d = '0';
+		c++;
+	}
+	return (0);
+}
