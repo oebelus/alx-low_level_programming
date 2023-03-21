749bc6537f1752d6db062c343b289a03c53d1276
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 18:10:23 +0000

    ll

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,19 +21,23 @@ void print_to_98(int n)
 			}
 			else
 			{
-				for (; n >= 98; n--)
-				{
-					if (n == 98)
-					{
-						printf("%d", n);
-						printf("\n");
-						break;
-					}
-					else
-					{
-						printf("%d", n);
-					}
-				}
+				printf("%d", n);
+			}
+		}
+	}
+	else
+		{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", n);
 			}
 		}
 	}
