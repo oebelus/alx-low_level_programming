2a17f8562897c302346d8fb18a567d2f6e31f353
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 18:00:37 +0000

    ll

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - if lowercase.
+ * @x a number
+ * @y a number
+ * Return: 1 if success 0 if failure.
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
