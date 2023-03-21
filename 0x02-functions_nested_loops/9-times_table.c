ece1ae8412c7ef576743b1e757694817a81fdf3d
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 17:57:19 +0000

    ll

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,9 +23,6 @@ void times_table(void)
 				_putchar(44);
 				_putchar(32);
 				_putchar(d + '0');
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
 				_putchar(u + '0');
 			}
 			else
