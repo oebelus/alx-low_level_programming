87465628d346474698cba52db7877e3a18534986
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 12:31:45 +0000

    ll

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@ void print_alphabet_x10(void)
 
 	int i = '0';
 
-	while (i < '9')
+	while (i <= '9')
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			putchar(c);
 		}
+		putchar('\n');
 		i++;
 	}
-	putchar('\n');
 }
