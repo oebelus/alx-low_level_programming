1f7336efe696d949ffa309ba41c6885fefa0cc4e
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 12:35:46 +0000

    ll

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: print_alphabet_x10 - print the alphabet.
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
