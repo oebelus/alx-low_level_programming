68a7ba2436618e3402044a82bdb98497914afddb
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 17:27:40 +0000

    ll

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * _abs - if lowercase.
+ * print_last_digit - if lowercase.
  * @n: is an integer.
  * Return: 1 if success 0 if failure.
 */
 
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	return(n % 10);
 }
