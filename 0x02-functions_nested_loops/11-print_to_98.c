4d18cc218ff60014809f44286673a0e242b4f682
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 18:05:05 +0000

    ll

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - if lowercase.
+ * @n: a number
+ * Return: 1 if success 0 if failure.
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", ", n);
+			}
+		}
+	}
+}
