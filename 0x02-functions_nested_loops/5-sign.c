2fd99b9f13c542831df8d8c0acb314ce87a71a66
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 13:06:12 +0000

    ll

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,10 +13,15 @@ int print_sign(int n)
 		putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
 	{
 		putchar('-');
-		return (0);
+		return (-1);
 	}
 		
 }
