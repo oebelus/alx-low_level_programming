5648b379471043e0a2d68f7098800c56445f8e1e
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 12:19:05 +0000

    ll

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,12 @@
  * Return: 0 if success
 */
 
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
 void print_alphabet(void)
 {
 	int c;
@@ -13,10 +19,4 @@ void print_alphabet(void)
 		putchar(c);
 	}
 	putchar('\n');
-}
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
