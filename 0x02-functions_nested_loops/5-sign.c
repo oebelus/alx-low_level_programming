ecfd9db067ef880c2ebc495969a03beca78ab489
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-21 13:15:50 +0000

    ll

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		printf("+1\n");
-		return (1);
 	}
 	else if (n == 0)
 	{
 		printf("00\n");
-		return (0);
 	}
 	else
 	{
 		printf("-1\n");
-		return (-1);
 	}
 		
 }
