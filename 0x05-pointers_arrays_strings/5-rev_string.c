88803637b9c86d14d11db222a5406b8474505d07
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-27 18:23:25 +0000

    ll

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,11 +11,11 @@ void rev_string(char *s)
 {
 	int len = strlen(s);
 	int i;
-	int tmp;
+	char tmp;
 
 	for (i = 0; i < len / 2; i++)
 	{
-		int tmp = s[i];
+		tmp = s[i];
 		s[len] = s[len - 1 - i];
 		s[len -1 - i] = tmp;
 	}
