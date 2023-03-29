9ceca265cfae2437b11ca8cf1411acafec4a0b9d
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-29 17:06:33 +0000

    ll

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,13 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	for (i; i < strlen(s); i++)
+	while (*(s + i) != '\0')
 	{
-		s[i] = toupper(s[i]);
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			*(s + i) = *(s + i) - 32;
+		}
+		i++;
 	}
 	return (s);
 }
