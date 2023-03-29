769c29c266b07ab1d03fa8cdb94dfcd5af0343bf
Imane <imaneelfadilelidrissi@gmail.com>
2023-03-29 17:29:54 +0000

    ll

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,32 @@ include main.h
  *
  * Return: String with all words capitalized.
  */
-
 char *cap_string(char *s)
 {
-	int string_count; 
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	string_count = 0;
-	while (s[string_count] != '\0')
+	i = 0;
+	while (*(s + i) != '\0')
 	{
-	if (s[0] >= 97 && s[0] <= 122)
-	{
-		s[0] = s[0] - 32;
-	}
-	if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n'
-	|| s[string_count] == ',' || string_count == ';' || string_count == '.'
-	 || string_count == '!' || string_count == '?' || string_count == '"'
-	 || string_count == '(' || string_count == ')' || string_count == '{'
-	 || string_count == '}')
-	{
-		if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			s[string_count + 1] = s[string_count + 1] - 32;
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
 		}
-	}
-		string_count++;
+	i++;
 	}
 	return (s);
 }
