e34289157224a0bdfe1f90486ad2611a22b63a1a
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-03 19:00:46 +0000

    ll

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,12 +14,16 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (i; i < strlen(s); i++)
+	while (*s)
 	{
-		if (s[i] == c)
-		{
-			return &s[i];
-		}
+		if (*s != c)
+			s++;
+		else
+			return (s);
 	}
-	return NULL;
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+	
 }
