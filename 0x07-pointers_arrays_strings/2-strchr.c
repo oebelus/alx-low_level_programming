477487e768a25a0e7a8e8ed72c9685d0e97becdf
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-03 19:01:32 +0000

    ll

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,29 +1,27 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
  * _strchr - locates a character
- * @s: the string
- * @c: the character to find
+ * @s: where to put copied buffer
+ * @c: source to copy from
+ * @n: memory area
  *
- * Return: s if yes, NULL if no.
+ * Return: NULL.
  */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (*s)
+	for (i; i < strlen(s); i++)
 	{
-		if (*s != c)
-			s++;
-		else
-			return (s);
+		if (s[i] == c)
+		{
+			return &s[i];
+		}
 	}
-	if (c == '\0')
-		return (s);
-
-	return (NULL);
-	
+	return NULL;
 }
