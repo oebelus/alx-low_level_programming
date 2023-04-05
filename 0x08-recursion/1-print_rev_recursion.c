36deadfb38aa6137f9ddb2223611c413a3115405
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-05 16:50:28 +0000

    ll

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints a string
+ *
+ * @s: the string
+ *
+ * Return: nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+		else
+		{
+			_print_rev_recursion(s+1);
+			printf("%c", *s);
+		}
+	}
+}
