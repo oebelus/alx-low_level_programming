86dd1f31556cccf5eceb93a061cfa9fd27e0eb11
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-05 17:10:04 +0000

    ll

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,16 +5,17 @@
  * _strlen_recursion - prints the length of a string
  *
  * @s: the string
+ * Return: return
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
