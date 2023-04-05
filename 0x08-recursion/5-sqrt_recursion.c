f526d8c166302b86428f403f2eb951551e0d052a
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-05 17:43:04 +0000

    ll

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - square root
+ *
+ * @n: the integer
+ * Return: return
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return -1;
+	}
+	if (n == 0 || n == 1)
+	{
+		return n;
+	}
+	int start = 1, end = n, ans = 0;
+
+	while (start <= end)
+	{
+		int mid = (start + end) / 2;
+		if (mid * mid == n)
+		{
+			return mid;
+		}
+		else if (mid * mid < n)
+		{
+			start = mid + 1;
+			ans = mid;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+	}
+	return ans;
+}
