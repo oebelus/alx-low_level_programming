e9c15bfc5c7da840b8e734933e0f272a3f2dd706
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-05 16:21:17 +0000

    ll

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+	puts('\n');
+}
