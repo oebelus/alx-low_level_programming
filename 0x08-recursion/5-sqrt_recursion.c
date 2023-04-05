0953b479e2184cee507eafaef141b87f037da2a1
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-05 18:05:34 +0000

    ll

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,31 +10,37 @@
 
 int _sqrt_recursion(int n)
 {
-	int mid = n / 2;
-	int result = _sqrt_recursion(mid);
-
 	if (n < 0)
 	{
 		return -1;
 	}
-	if (n == 0 || n == 1)
+	else if (n == 0 || n == 1)
 	{
 		return n;
 	}
- 	if (result == -1)
-	{
-		return -1;
-	}
-	if (result * result <= n && (result + 1) * (result + 1) > n)
-	{
-		return result;
-	}
-	if (result * result > n)
-	{
-		return _sqrt_recursion(n - mid);
-	}
 	else
 	{
-		return _sqrt_recursion(n);
-    	}
+		int mid = n / 2;
+		int result = _sqrt_recursion(mid);
+		if (result == -1)
+		{
+			return -1;
+		}
+		else if (result * result == n)
+		{
+			return result;
+		}
+		else if (result * result < n && (result + 1) * (result + 1) > n)
+		{
+			return result;
+		}
+		else if (result * result > n)
+		{
+			return _sqrt_recursion(mid);
+		}
+		else
+		{
+			return _sqrt_recursion(n - mid);
+		}
+	}
 }
