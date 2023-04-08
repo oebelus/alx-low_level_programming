9313cd4213b414ebb1aed86a226375915e39a3e2
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-08 15:33:52 +0000

    ll

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+    printf("Program name: %s\n", argv[0]);
+    return (0);
+}
