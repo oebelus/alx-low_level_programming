5046c11f382d7c7fbb88cc19fca46e982872d977
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-09 19:10:07 +0000

    ll

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,15 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * check_num - check if a string represents a valid integer
- * @str: string to check
- * Return: 1 if valid int, 0 otherwise
+ * _atoi - string to int
+ * @s: string to use
+ * Return: int
  */
 
 int _atoi(char *s)
 {
+	return (s);
 }
