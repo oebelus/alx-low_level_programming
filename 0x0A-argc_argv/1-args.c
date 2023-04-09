24b4e143ebda6f40299e578808a3b07d31b4aa99
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-09 18:00:02 +0000

    ll

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - prints the name of the program
@@ -10,6 +11,6 @@
 int main(int argc, char *argv[])
 {
 	(void) argv;
-	printf("d%\n", argc - 1);
+	printf("d%\n", argc);
 	return (0);
 }
