0b20b1b5d72f06b0a24c538397389630098615af
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-09 18:10:32 +0000

    ll

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of command line argume,ts
+ * @argv: arrray name
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	int a,b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a*b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
