133dc7a6e6e900f5843334b09f0eb02753c9db58
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-11 18:35:22 +0000

    ll

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* create_array - creates an array of chars
+*@size: Size array
+*@c: charto insert
+*Return: NULL if failure, array if success
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
