0c69a53649b8392e5315f7acfd43763c31877833
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-19 17:47:50 +0000

    ll

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: name
+ * @f: function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
