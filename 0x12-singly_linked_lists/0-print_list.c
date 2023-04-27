6f01c50d93db4d9fac35f506dc2996d47b4d4dec
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-27 14:54:37 +0100

    ll

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@ include "lists.h"
 
 /**
  * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * h: The list_t list.
  *
  * Return: The number of nodes in h.
  */
@@ -16,6 +16,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
