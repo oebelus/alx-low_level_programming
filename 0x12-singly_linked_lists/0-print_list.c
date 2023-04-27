f15d87276a5167aeb57d5cdaa386cd812046edfc
Imane <imaneelfadilelidrissi@gmail.com>
2023-04-27 14:44:46 +0100

    ll

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - print all the elements
+ * @h: pointer to the head to the list
+ * Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = head; // pointer to the head
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%s\n", current->str);
+		}
+		current = curret->next
+	}
+}
