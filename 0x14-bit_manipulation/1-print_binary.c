811304c78a31d96d270e89e2ab6559d9383bae98
Imane <imaneelfadilelidrissi@gmail.com>
2023-07-06 21:05:34 +0100

    'll'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * binary_to_uint - binary to decimal
- * @b: pointer to the character
+ * print_binary - decimal to binary
+ * @n: integer to convert
  */
 
 void print_binary(unsigned long int n)
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	{
 		if (n > 1)
 			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+		_putchar((n & 1) + '0'); # To convert to char
 	}
 	else
 	{
