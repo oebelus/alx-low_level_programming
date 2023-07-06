4b2f372b8e16b8e297fdcc2bc8d3a225f33edf23
Imane <imaneelfadilelidrissi@gmail.com>
2023-07-06 21:03:48 +0100

    'll'

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,20 +1,20 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * print_binary - decimal to binary
- * @n: integer to convert
+ * binary_to_uint - binary to decimal
+ * @b: pointer to the character
  */
 
 void print_binary(unsigned long int n)
 {
 	if (n > 0)
 	{
-		print_binary(n >> 1);
-		putchar((n & 1) + '0');
+		if (n > 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 	}
 }
