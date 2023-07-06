f1b54dcbcc846066c6262edd1d51404b9b8fa2fd
Imane <imaneelfadilelidrissi@gmail.com>
2023-07-06 22:29:41 +0100

    'll'

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,15 @@
 #include "main.h"
-#include "string.s"
 
 /**
  * get_bit - decimal to binary
  * @n: pointer to integer
  * @index: index of the char
+ * Return: The value of the bit at the given index
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > sizeof(n) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 	return ((n >> index) & 1);
 }
