178d40b9c812f04d5c855fca8da00de504b69194
Imane <imaneelfadilelidrissi@gmail.com>
2023-07-10 17:10:23 +0100

    'll'

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+/**
+ * binary_to_uint - binary to decimal
+ * @b: pointer to the character
+ * Return: 0 if b is NULL, or b is not 0 nor 1
+ */
+
+int create_file(const char *filename, const char *text_content)
+{
+	if (filename)
+	{
+		FILE *file = fopen(filename, "w");
+		if (file)
+		{
+			fprintf(file, "%s", text_content);
+			return (1);
+		}
+	}
+	return (0);
+}
