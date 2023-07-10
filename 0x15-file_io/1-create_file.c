4c5be5b1b83257a878cdeb576ab3dc9b6ebc0cbc
Imane <imaneelfadilelidrissi@gmail.com>
2023-07-10 17:32:07 +0100

    'll'

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,17 +8,35 @@
  * Return: -1 if failure, 1 if success
  */
 
-int create_file(const char *filename, const char *text_content)
+int create_file(const char *filename, char *text_content)
 {
-	if (filename)
+	int fd, len, write_bytes;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
 	{
-		FILE *file = fopen(filename, "w");
-		if (file)
-		{
-			fprintf(file, "%s", text_content);
-			fclose(file);
-			return (1);
-		}
+		close(fd);
+		return (1);
 	}
-	return (-1);
+
+	for (len = 0; text_content[len]; len++)
+		;
+
+	write_bytes = write(fd, text_content, len);
+	if (write_bytes == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	/* close file and return 1 on success */
+	close(fd);
+	return (1);
 }
+
